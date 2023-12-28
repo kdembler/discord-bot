ffb1ab325b3de261bee75de37ab7166c278a1659
Klaudiusz Dembler <accounts@kdembler.com>
2023-12-28 14:48:45 +0100

    update help message

--- Joystream-Role-Bot/src/commands/Help.ts
+++ Joystream-Role-Bot/src/commands/Help.ts
@@ -6,7 +6,7 @@ export const Help: Command = {
   description: "Help page for the Joystream discord role bot",
 
   run: async (client: Client, interaction: CommandInteraction) => {
-    const content = `Please follow this link to learn how to use the Joystream role bot. https://www.notion.so/joystream/Joystream-Role-Bot-c090b6d5f54c40509a736eb0a6201834?pvs=4    `;
+    const content = `Please follow this link to learn how to use the Joystream role bot. https://joystream.notion.site/Joystream-Role-Bot-c090b6d5f54c40509a736eb0a6201834?pvs=4`;
 
     await interaction.followUp({
       ephemeral: true,
